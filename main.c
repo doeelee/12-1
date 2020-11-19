ca638dedd1230b166377e0dbe20fd30b498b5fe0
doelele <doelele@users.noreply.github.com>
2020-11-19 15:43:11 +0900

    12-3, 4

--- main.c
+++ main.c
@@ -5,23 +5,22 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i = 0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	printf("%s\n", str);
 	
-	while (str[i] != '\0') {
-		
-	    printf("%c", str[i]);	
-	    i++;
+	char str[30] = "happy C programming";
 	
-	}
 	
+	strcpy(dst, src);
+	
+	printf("copied string : %s\n", dst);
+	
+	printf("length of \"%s\" : %i\n", str, strlen(str));
+	
+
 	
 	
 	return 0;
 }
+
